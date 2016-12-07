c20357bf24b26c4d2a270b17734bbc2924ed52dd
Unknown <201504530@ingenieria.usac.edu.gt>
2016-12-07 01:29:42 -0600

    Metodo 3:
    En el boton 3 realiza la operacion = x*100 -y + (x^y),    ademas agrega los datos que el usuario escribio.

--- Hoja de Trabajo1/Form1.cs
+++ Hoja de Trabajo1/Form1.cs
@@ -50,5 +50,22 @@ namespace Hoja_de_Trabajo1
                 label5.Text = "datos incorrectos";
             }
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                x = int.Parse(textBox1.Text);
+                y = int.Parse(textBox2.Text);
+                resp = x*100 -y + (x^y);
+                label5.Text = "x*100 -y + (x^y)";
+                richTextBox1.Text = Convert.ToString(resp) + " \n usted escribio x = "+ x +
+                    " \n usted escribio y = " + y;
+            }
+            catch
+            {
+                label5.Text = "datos incorrectos";
+            }
+        }
     }
 }
